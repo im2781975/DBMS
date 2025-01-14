f565acaab3bf8d854b88bd7d4820fe9ec2d3e374
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-01-14 20:56:00 +0600

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -20,46 +20,54 @@ BEGIN
     INSERT INTO StudentLog (student_id)
     VALUES (NEW.id);
 END;
-"""              project                """
+CREATE DATABASE phitron;
+USE phitron;
 CREATE TABLE student(
-    serial int PRIMARY KEY,
-    name VARCHAR(20), age INT );
-INSERT INTO student(serial, name, age)
-VALUES(1, 'Ankit', 17), (2, 'Ramya', 18), (3, 'Ram', 16);
-select * From student;
-CREATE TABLE course(
-    course_id INT, name VARCHAR(20) );
-INSERT INTO student(course_id, name)
-VALUES(101, "python"), (102, " CPP"), (103, "DBMS");
-select * From course;
-CREATE TABLE enroll(
-    serial INT, course_id INT, admit_date date,
-    PRIMARY KEY(serial, course_id),
-    FOREIGN KEY(serial) REFERENCES student(serial)
-        ON DELETE CASCADE,
-    FOREIGN KEY(course_id) REFERENCES course(course_id)
-        ON DELETE CASCADE
+    roll INT, name VARCHAR(30)
 );
-INSERT INTO enroll(serial, course_id, admit_date)
-VALUES(1, 101, "06-01-21"), (1, 102, "06-01-21"), (2, 103, "06-01-21");
-select * from enroll
-SET SQL_SAFE_UPDATES = 0;
-DELETE from student where name = "mollavai"
-SET SQL_SAFE_UPDATES = 1;
-"""               project               """
+DROP TABLE student;
+"""                                """
 CREATE TABLE student(
-    serial INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL );
-CREATE TABLE course(
-    course_id INT, course_name VARCHAR(30),
-    PRIMARY KEY(course_id)
+    roll INT, name VARCHAR(30), contact CHAR(11), Email VARCHAR(30)
 );
-CREATE TABLE enroll(
-    serial INT, course_id INT, admitDate date,
-    PRIMARY KEY(serial, course_id),
-    FOREIGN KEY(serial) REFERENCES student,
-    FOREIGN KEY(course_id) REFERENCES course
+INSERT INTO student(roll, name, contact, Email)
+values(101, "Aa", "01234567890", "Aa@gmail.com");
+INSERT INTO student(roll, name, Email)
+values(102, "Bb", "Bb@gmail.com");
+INSERT INTO student(roll, name, contact, Email)
+values(103, "Dd", "01234567890", "Dd@gmail.com");
+INSERT INTO student(roll, name, contact, Email)
+values(104, "Ee", "01234567890", "Ee@gmail.com");
+INSERT INTO student(roll, name, contact, Email)
+values(105, "Ff", "01234567890", "Ff@gmail.com");
+SELECT * from student
+DELETE from student where roll = 102
+"""            UPGRADES                """
+SET SQL_SAFE_UPDATES = 0
+UPDATE student
+SET name = "Cc", contact = "00000000000" where roll = 101
+SET SQL_SAFE_UPDATES = 1
+SET SQL_SAFE_UPDATES = 0
+DELETE from student where roll = 105
+SET SQL_SAFE_UPDATES = 1
+"""            CONSTRAINT               """
+CREATE TABLE student(
+roll INT NOT NULL, UNIQUE, PRIMARY KEY, 
+  name varchar(30) NOT NULL,
+  /* NOT NULL(name), */
+  contact CHAR(11) UNIQUE,
+  Email VARCHAR(30) UNIQUE,
+  Age INT Default '13' CHECK (Age >= 18)
+  CONSTRAINT student_check CHECK(Age >= 18 AND name = 'Molla')
 );
+"""                QUERY                """
+SELECT name, roll from student where roll = 103;
+SELECT name from student where roll > (
+    SELECT name from student where roll = 104
+    );
+SELECT name from student where roll != 102 AND roll > (
+    select name from student where roll = 102
+    );
 """             Rename Table            """
 RENAME TABLE student to person;
 ALTER TABLE student RENAME person;
