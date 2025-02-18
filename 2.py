4b12e3a919a1f46d9c3c1af9b31d8efa84d070eb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-18 07:33:09 +0600

    Update 2.py

--- 2.py
+++ 2.py
@@ -100,5 +100,22 @@ CREATE TABLE BooksAuthors(
 );
 INSERT INTO BooksAuthors(BookId, AuthorId)
 VALUES (1, 1), (2, 1), (3, 1), (4, 2), (5, 2), (6, 3), (7, 4), (7, 5), (7, 6),    (7, 7), (7, 8) ;
-
+--            Countries table
+CREATE TABLE Countries ( 
+    Id INT NOT NULL AUTO_INCREMENT,    
+    ISO VARCHAR(2) NOT NULL,    
+    ISO3 VARCHAR(3) NOT NULL,    
+    ISONumeric INT NOT NULL,    
+    CountryName VARCHAR(64) NOT NULL,    
+    Capital VARCHAR(64) NOT NULL,    
+    ContinentCode VARCHAR(2) NOT NULL,    
+    CurrencyCode VARCHAR(3) NOT NULL,    
+    PRIMARY KEY(Id) ) ;
+INSERT INTO Countries (ISO, ISO3, ISONumeric, CountryName, Capital, ContinentCode, CurrencyCode) VALUES 
+    ('AU', 'AUS', 36, 'Australia', 'Canberra', 'OC', 'AUD'),   
+    ('DE', 'DEU', 276, 'Germany', 'Berlin', 'EU', 'EUR'),   
+    ('IN', 'IND', 356, 'India', 'New Delhi', 'AS', 'INR'),   
+    ('LA', 'LAO', 418, 'Laos', 'Vientiane', 'AS', 'LAK'),   
+    ('US', 'USA', 840, 'United States', 'Washington', 'NA', 'USD'),   
+    ('ZW', 'ZWE', 716, 'Zimbabwe', 'Harare', 'AF', 'ZWL')
 
